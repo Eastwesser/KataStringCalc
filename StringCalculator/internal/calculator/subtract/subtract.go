7aacac30d995fad3ee716a9cc744051e2b5c5366
Dennis Mattews <alttert@mail.ru>
2024-08-02 20:25:04 +0300

    trim and subtract upd

--- StringCalculator/internal/calculator/subtract/subtract.go
+++ StringCalculator/internal/calculator/subtract/subtract.go
@@ -7,8 +7,11 @@ import (
 
 // Subtract Удаляет все вхождения второй строки из первой и вызывает trimStringsAfter40.
 func Subtract(a, b string) string {
+	// Проверяем, содержит ли строка a строку b
 	if !strings.Contains(a, b) {
+		// Если строка a не содержит строку b, обрезаем строку a до 40 символов и возвращаем её
 		return trim.TrimStringsAfter40(a)
 	}
+	// Если строка a содержит строку b, удаляем все вхождения строки b из строки a
 	return trim.TrimStringsAfter40(strings.ReplaceAll(a, b, ""))
 }
