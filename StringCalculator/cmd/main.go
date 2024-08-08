5375c3ae3815a1656bd3c7083c02564003adce5c
Dennis Mattews <alttert@mail.ru>
2024-08-08 12:38:09 +0300

    main func clear

--- StringCalculator/cmd/main.go
+++ StringCalculator/cmd/main.go
@@ -9,24 +9,18 @@ import (
 )
 
 func main() {
-	// Создаем новый сканер для считывания ввода
 	scanner := bufio.NewScanner(os.Stdin)
 
-	// Запрос ввода от пользователя
 	fmt.Print("Введите выражение: ")
 
-	// Считываем всю строку ввода
 	scanner.Scan()
 	input := scanner.Text()
 
-	// Проверяем, что ввод не пустой и содержит хотя бы один пробел
 	if strings.TrimSpace(input) == "" || !strings.Contains(input, " ") {
 		fmt.Println("Ошибка! Введите валидное выражение!!!")
 		return
 	}
 
-	// Вызываем функцию Calculate из пакета calculator с введенным выражением
 	result := calculator.Calculate(input)
-	// Выводим результат на экран
 	fmt.Println(result)
 }
